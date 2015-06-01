044333a064d9547624ba3c2fd9f5f6b406a9549b
Francisco Penedo <fran.penedo@gmail.com>
2015-06-01 19:46:19 -0400

    Added mc as default file manager

--- default.sh
+++ default.sh
@@ -4,3 +4,4 @@ xdg-mime default evince.desktop application/pdf
 xdg-mime default gvim.desktop $(grep '^text/x-*' /usr/share/mime/types)
 xdg-mime default gvim.desktop text/plain
 xdg-mime default nomacs.desktop $(grep '^image*' /usr/share/mime/types | grep -v 'x-*')
+xdg-mime default MidnightCommander.desktop inode/directory
