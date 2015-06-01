ec590c3e9755f58fae68fc18451c6d32ce821d67
Francisco Penedo <fran.penedo@gmail.com>
2015-06-01 17:21:33 -0400

    Added more mime types

--- default.sh
+++ default.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 xdg-mime default evince.desktop application/pdf
+xdg-mime default gvim.desktop $(grep '^text/x-*' /usr/share/mime/types)
+xdg-mime default gvim.desktop text/plain
+xdg-mime default nomacs.desktop $(grep '^image*' /usr/share/mime/types | grep -v 'x-*')
