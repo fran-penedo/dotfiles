fad21e21fafd8b4f39b31305f59140a3f4bff13a
Francisco Penedo <fran.penedo@gmail.com>
2015-06-18 19:19:45 -0400

    More sup configuration
    Offlineimap runs now as daemon and is launched by i3 at start.
    Added auto labels and sound notification to sup.

---
+++ sup/hooks/after-poll.rb
@@ -0,0 +1,3 @@
+if num_inbox > 0
+    system("pacmd play-file /usr/share/sounds/freedesktop/stereo/message-new-instant.oga 1")
+end
--- sup/hooks/before-add-message.rb
+++ sup/hooks/before-add-message.rb
@@ -1,1 +1,29 @@
-# Test
+if message.from.email.end_with?('mariaines.dff@gmail.com', 'pacopenedo@yahoo.es', 'francisco.penedo@meyss.es', 'pacopenedo@hotmail.es')
+    message.add_label :personal
+end
+
+if message.recipients.any? { |person| person.email.start_with? 'francisco.penedo@estudiante.uam.es' }
+    message.add_label :uam
+end
+
+if message.recipients.any? { |person| person.email.start_with? 'franp@bu.edu' }
+    message.add_label :bu
+end
+
+if message.from.email.end_with? '@bu.edu' 
+    message.add_label :bu
+end
+
+if message.from.email.end_with?('@bpl.org', '@bibliocommons.com', '@minlib.net') or message.recipients.any? { |person| person.email.end_with? '@bpl.org' }
+    message.add_label :library
+end
+
+if message.from.email.end_with? '@bbva.com' 
+    message.add_label :bbva
+end
+
+if message.from.email.end_with? '@alerts.santander.us'
+    message.add_label :santander
+end
+
+
