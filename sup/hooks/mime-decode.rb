6eab96571479fe0fd3ab5e8e5250ca2180f54597
Francisco Penedo <fran.penedo@gmail.com>
2016-02-03 18:49:32 -0500

    sup: display link numbers when decoding html

--- sup/hooks/mime-decode.rb
+++ sup/hooks/mime-decode.rb
@@ -2,6 +2,6 @@ require 'shellwords'
 unless sibling_types.member? "text/plain"
     case content_type
     when "text/html"
-    `/usr/bin/w3m -dump -T #{content_type} #{Shellwords.escape filename}`
+    `/usr/bin/w3m -dump -o display_link_number=1 -T #{content_type} #{Shellwords.escape filename}`
     end
 end
