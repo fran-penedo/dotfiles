0b304d6e7fa048b5b340707cdd6b81ff8896964d
Francisco Penedo <fran.penedo@gmail.com>
2020-05-13 17:05:42 -0400

    i3: Added better now_playing module

--- i3/i3status2.py
+++ i3/i3status2.py
@@ -2,18 +2,26 @@
 
 import subprocess
 
-from i3pystatus import Status
+from i3pystatus import Status, now_playing
+
+
+class MyNowPlaying(now_playing.NowPlaying):
+    def __init__(self):
+        _format = (
+            "{title} - {artist} | Vol. {volume} | {song_elapsed}/{song_length} {status}"
+        )
+        super().__init__(format=_format)
+
+    def volume_up(self):
+        self.set_player_prop("Volume", self.volume + 0.02)
+
+    def volume_down(self):
+        self.set_player_prop("Volume", self.volume - 0.02)
+
 
 status = Status(standalone=True)
 
-# status.register("cmus",)
-
-status.register("spotify",
-    format="{title} - {artist} {status}",
-    # max_len=130,
-    # max_field_len=70,
-    on_upscroll=["player_command", "volume +2"],
-    on_downscroll=["player_command", "volume -2"])
+status.register(MyNowPlaying())
 
 status.register(
     "mpd",
@@ -21,6 +29,7 @@ status.register(
     max_len=130,
     max_field_len=70,
     on_upscroll=["mpd_command", "volume +2"],
-    on_downscroll=["mpd_command", "volume -2"])
+    on_downscroll=["mpd_command", "volume -2"],
+)
 
 status.run()
