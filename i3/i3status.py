43ff0af515121cfdc484851c426840120d695cea
Francisco Penedo <fran.penedo@gmail.com>
2020-05-20 14:09:40 -0400

    astroid: added inbox tab, added inbox label to i3 panel filter

--- i3/i3status.py
+++ i3/i3status.py
@@ -10,32 +10,42 @@ status = Status(standalone=True)
 # Displays clock like this:
 # Tue 30 Jul 11:59:46 PM KW31
 #                          ^-- calendar week
-status.register("clock",
-                format="%a %-d %b %H:%M:%S ",)
+status.register(
+    "clock", format="%a %-d %b %H:%M:%S ",
+)
 
-status.register("battery",
-                battery_ident="ALL",
-                format="{status} {percentage:0.0f}% {remaining}",)
+status.register(
+    "battery", battery_ident="ALL", format="{status} {percentage:0.0f}% {remaining}",
+)
 
-status.register("disk", path="/", round_size=1,
-                format="/:{used}/{total}",)
+status.register(
+    "disk", path="/", round_size=1, format="/:{used}/{total}",
+)
 
-status.register("disk", path="/home", round_size=1,
-                format="/home:{used}/{total}",)
+status.register(
+    "disk", path="/home", round_size=1, format="/home:{used}/{total}",
+)
 
 
-status.register("cpu_usage",
-                format="CPU:{usage:02}%",)
+status.register(
+    "cpu_usage", format="CPU:{usage:02}%",
+)
 
-status.register("mem",
-                format="MEM:{used_mem}M",)
+status.register(
+    "mem", format="MEM:{used_mem}M",
+)
 
-status.register("mail", hide_if_null=False,
-                backends=[notmuchmail.Notmuch(query='tag:unread and -tag:spam')])
+status.register(
+    "mail",
+    hide_if_null=False,
+    backends=[notmuchmail.Notmuch(query="tag:inbox and tag:unread and -tag:spam")],
+)
 
-status.register("network",
-                format_up="{interface} u:{bytes_sent}KB/s d:{bytes_recv}KB/s",
-                interface="wlp5s0")
+status.register(
+    "network",
+    format_up="{interface} u:{bytes_sent}KB/s d:{bytes_recv}KB/s",
+    interface="wlp5s0",
+)
 
 status.register("openvpn", vpn_name="client", use_new_service_name=True)
 
