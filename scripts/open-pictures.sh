0500707fa044f664b59e41e20ee93484526957c1
Francisco Penedo <fran@franpenedo.com>
2021-09-10 20:00:54 +0200

    Fixed open pictures script

--- scripts/open-pictures.sh
+++ scripts/open-pictures.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 CAMDIR=~/cloud/Photos/Camera
-YEARDIR=$CAMDIR/$(ls --reverse $CAMDIR | head -n 1)
-MONTHDIR=$YEARDIR/$(ls --reverse $YEARDIR | head -n 1)
+YEARDIR=$(echo $CAMDIR/*(/On[1]))
+MONTHDIR=$YEARDIR/*(/On[1])
 
-nomacs $MONTHDIR
+nomacs $~MONTHDIR
