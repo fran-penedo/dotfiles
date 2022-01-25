19e64cd708aa7ad049c2ac3de587a11e6092131e
Francisco Penedo <fran@franpenedo.com>
2022-01-25 14:55:40 +0100

    archivebox: switch to docker-compose

--- scripts/archivebox-periodic.sh
+++ scripts/archivebox-periodic.sh
@@ -3,5 +3,5 @@
 last=$(systemctl --user show --property=LastTriggerUSec archivebox-periodic.timer | sed 's/.*=//')
 [[ ${last} -eq '0' ]] && last="last month"
 
-/home/fran/dotfiles/scripts/export_browser_history.sh -d 7 -s "${last}" ~/.mozilla/firefox/3exufmp6.default-release/places.sqlite | /home/fran/dotfiles/scripts/filter_links.py | archivebox add
+/home/fran/dotfiles/scripts/export_browser_history.sh -d 7 -s "${last}" ~/.mozilla/firefox/3exufmp6.default-release/places.sqlite | /home/fran/dotfiles/scripts/filter_links.py | docker-compose run archivebox add
 pkill -f 'chromium --headless'
