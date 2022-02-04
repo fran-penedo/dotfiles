24736fc5641f618128856d2f60f2c30a795a5dda
Francisco Penedo <fran@franpenedo.com>
2022-02-04 02:29:39 +0100

    i3: add default filters for lnav session

---
+++ lnav/formats/installed/journald.sql
@@ -0,0 +1,3 @@
+INSERT INTO lnav_view_filters (view_name, enabled, type, pattern) VALUES ('log', 1, 'OUT', 'poll script:');
+INSERT INTO lnav_view_filters (view_name, enabled, type, pattern) VALUES ('log', 1, 'OUT', 'GET /admin/login/ HTTP/1');
+INSERT INTO lnav_view_filters (view_name, enabled, type, pattern) VALUES ('log', 1, 'OUT', 'run-docker-runtime\\x2drunc-moby');
