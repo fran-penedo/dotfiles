36c1009beb0dd12f3f8463a85666768bb2325d20
Francisco Penedo <fran@franpenedo.com>
2022-02-08 02:02:06 +0100

    Fix docker-compose ignoring piped input in archivebox script
    Added actual exit code so systemd has a better idea of what happened

--- scripts/archivebox-periodic.sh
+++ scripts/archivebox-periodic.sh
@@ -3,5 +3,10 @@
 last=$(systemctl --user show --property=LastTriggerUSec archivebox-periodic.timer | sed 's/.*=//')
 [[ ${last} -eq '0' ]] && last="last month"
 
-/home/fran/dotfiles/scripts/export_browser_history.sh -d 7 -s "${last}" ~/.mozilla/firefox/3exufmp6.default-release/places.sqlite | /home/fran/dotfiles/scripts/filter_links.py | docker-compose run archivebox add
+/home/fran/dotfiles/scripts/export_browser_history.sh -d 7 -s "${last}" ~/.mozilla/firefox/3exufmp6.default-release/places.sqlite | /home/fran/dotfiles/scripts/filter_links.py | docker-compose run -T archivebox add
+
+exit_code=$?
+
 pkill -f 'chromium --headless'
+
+exit $exit_code
